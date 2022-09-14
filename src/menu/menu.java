7db4ae0f583b5356b217ef451b2802ffa5a4df8d
antaww <anta.pizzetta@gmail.com>
2022-09-14 11:51:14 +0200

    first commit

---
+++ src/menu/menu.java
@@ -0,0 +1,39 @@
+package menu;
+
+import java.util.Scanner;
+
+public class menu {
+    public static void main(String[] args) {
+        System.out.println("Choisissez un exercice :");
+        System.out.println("1. Exercice 1 - Hello World");
+        System.out.println("2. Exercice 2");
+        System.out.println("3. Exercice 3");
+        System.out.println("4. Exercice 4");
+        System.out.println("0. Quitter");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("> ");
+        String choix = scanner.nextLine();
+        switch (choix) {
+            case "1" -> System.out.print("Exercice 1");
+            case "2" -> System.out.print("Exercice 2");
+            case "3" -> System.out.print("Exercice 3");
+            case "4" -> System.out.print("Exercice 4");
+            case "0" -> {
+                System.out.print("Au revoir");
+                for (int i = 0; i < 3; i++) {
+                    try {
+                        Thread.sleep(400);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.print(".");
+                }
+                System.exit(0);
+            }
+            default -> {
+                System.out.println("Choix invalide");
+                main(args);
+            }
+        }
+    }
+}
