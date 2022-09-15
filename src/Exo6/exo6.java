d887c476e2e801063446179f890654b0bbe87d50
Bahsiik <o.mist1303@gmail.com>
2022-09-15 17:07:21 +0200

    exo6 adjust

--- src/Exo6/exo6.java
+++ src/Exo6/exo6.java
@@ -2,16 +2,18 @@ package Exo6;
 
 public class exo6 {
     public static void main(String[] args) {
-        System.out.println("Entrez la taille du tableau :");
+        System.out.println("Entrez la taille du tableau (0 pour terminer) :");
         int taille = utils.utils.getPositiveInt();
+        if (taille == 0) {
+            utils.utils.SystemOut();
+        }
         double[][] tableau = new double[taille][taille];
         for (int i = 0; i < taille; i++) {
             for (int j = 0; j < taille; j++) {
                 System.out.println("Entrez la valeur de la case [" + (i+1) + "] [" + (j+1) + "] :");
-                tableau[i][j] = utils.utils.getInt();
+                tableau[i][j] = utils.utils.getDouble();
             }
         }
-
         System.out.println("\nVoici le tableau :");
         double total = 0;
         double rowCounter = 0;
@@ -34,5 +36,6 @@ public class exo6 {
             colCounter = 0;
         }
         System.out.print("(" + total + ")");
+        exo6.main(args);
     }
 }
