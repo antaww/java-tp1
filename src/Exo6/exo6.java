7e54635290dd0c39699cd9e2981017fb5650f0a5
Bahsiik <o.mist1303@gmail.com>
2022-09-27 12:19:20 +0200

    fix print for exo 6

--- src/Exo6/exo6.java
+++ src/Exo6/exo6.java
@@ -10,9 +10,11 @@ import java.util.*;
 import utils.utils;
 
 public class exo6 {
+
     /**
-     * Il demande à l'utilisateur la taille du tableau, puis demande les valeurs de chaque cellule, puis imprime le tableau
-     * avec la somme de chaque ligne et colonne, et enfin la somme de l'ensemble du tableau
+     * Il demande à l'utilisateur la taille du tableau, puis demande les valeurs de chaque cellule, puis imprime le
+     * tableau, puis imprime la moyenne de chaque ligne et colonne, puis imprime le total des moyennes des lignes et des
+     * colonnes
      */
     public static void main(String[] args) {
         System.out.println("Entrez la taille du tableau (0 pour terminer) :");
@@ -23,7 +25,7 @@ public class exo6 {
         double[][] tableau = new double[taille][taille];
         for (int i = 0; i < taille; i++) {
             for (int j = 0; j < taille; j++) {
-                System.out.println("Entrez la valeur de la case [" + (i+1) + " , " + (j+1) + "] :");
+                System.out.println("Entrez la valeur de la case [" + (i + 1) + " , " + (j + 1) + "] :");
                 tableau[i][j] = utils.getDouble();
             }
         }
@@ -38,7 +40,8 @@ public class exo6 {
                 rowAverage += tableau[i][j];
             }
             rowAverage /= taille;
-            System.out.println("\u001B[31m"+ "(" + Math.round( rowAverage * 100.0) / 100.0  + ")"+"\u001B[0m");
+
+            System.out.printf("\u001B[33m" + "%-10.2f" + "\u001B[0m", Math.round(rowAverage * 100.0) / 100.0);
             rowTotal += rowAverage;
             rowAverage = 0;
             System.out.println();
@@ -48,12 +51,15 @@ public class exo6 {
                 colAverage += tableau[j][i];
             }
             colAverage /= taille;
-            System.out.print("\u001B[35m"+ "(" + Math.round( colAverage * 100.0) / 100.0  + ")"+"\u001B[0m");
+            System.out.printf("\u001B[35m" + "%-10.2f" + "\u001B[0m", Math.round(colAverage * 100.0) / 100.0);
             colTotal += colAverage;
             colAverage = 0;
         }
-        System.out.print("row(" + Math.round( rowTotal * 100.0) / 100.0 + ")");
-        System.out.print("col(" + Math.round( colTotal * 100.0) / 100.0 + ")");
+        System.out.println("\u001B[35m\n\nTotal de la moyenne des colonnes : ");
+        System.out.printf("%-10.2f" + "\u001B[0m", Math.round(colTotal * 100.0) / 100.0);
+        System.out.println();
+        System.out.println("\u001B[33mTotal de la moyenne des lignes : ");
+        System.out.printf("%-10.2f" + "\u001B[0m", Math.round(rowTotal * 100.0) / 100.0);
         utils.Continue(utils.getExoNumber());
     }
 }
